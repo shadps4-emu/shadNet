075c4bd3b4e28d0324a56ee9ebe50f63caa3101c
georgemoralis <giorgosmrls@gmail.com>
2026-04-26 09:40:43 +0300

    added timestamps

--- src/main.cpp
+++ src/main.cpp
@@ -4,17 +4,29 @@
 #include <QCoreApplication>
 #include <QDebug>
 #include <QDir>
+#include <QLoggingCategory>
 #include "config.h"
 #include "server.h"
 
+const QString versionString = QStringLiteral("0.0.1");
+
 int main(int argc, char* argv[]) {
+    QLoggingCategory::setFilterRules(QStringLiteral("*.debug=false\n*.info=true"));
+    qSetMessagePattern(QStringLiteral("%{time yyyy-MM-dd HH:mm:ss.zzz}  "
+                                      "%{if-debug}DEBUG%{endif}"
+                                      "%{if-info} INFO%{endif}"
+                                      "%{if-warning} WARN%{endif}"
+                                      "%{if-critical} CRIT%{endif}"
+                                      "%{if-fatal}FATAL%{endif}"
+                                      "  %{if-category}[%{category}] %{endif}%{message}"));
+
     QCoreApplication app(argc, argv);
     app.setApplicationName("shadnet");
 
     // Set working directory to executable location
     QDir::setCurrent(QCoreApplication::applicationDirPath());
 
-    qInfo() << "ShadNet Qt server";
+    qInfo() << "ShadNet Qt server version" << versionString;
 
     ConfigManager config;
     config.Load();
